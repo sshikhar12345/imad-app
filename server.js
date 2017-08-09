c0a2aaf544a4b0220b18edc102afe0f9a1662ac1
sshikhar12345 <30680995+sshikhar12345@users.noreply.github.com>
2017-08-09 12:10:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/article-three', function(req,res)
 {
    res.send('Article three requested!');  
 });
+app.get('ui/main/js', function(req,res)
+{
+   res.sendFile(path.join(__dirname, 'ui','main.js'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
